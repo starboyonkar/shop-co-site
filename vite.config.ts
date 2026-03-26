4ea343ec6e3765146b50c0ee98961cc956296224
starboyonkar <onkarchougule501@gmail.com>
2026-03-26 20:11:25 +0530

    updated package.json and vite file

--- vite.config.ts
+++ vite.config.ts
@@ -13,7 +13,7 @@ export default defineConfig(({ mode }) => ({
     },
   },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
-  base: "/shop-co-site/",
+  base: '/shop-co-site/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
